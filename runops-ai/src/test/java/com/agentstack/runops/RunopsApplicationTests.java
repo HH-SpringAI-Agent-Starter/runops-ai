574046e7a84178839e0a7a43291ed5f2dabfcb27
javaAgentAI <3159979021@qq.com>
2026-07-05 20:14:58 +0800

    chore: update runops-ai/src/test/java/com/agentstack/runops/RunopsApplicationTests.java [skip ci]

---
+++ runops-ai/src/test/java/com/agentstack/runops/RunopsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.agentstack.runops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
